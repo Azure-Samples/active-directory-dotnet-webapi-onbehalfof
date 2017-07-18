959693e051d884e8a1fe1772c5da99c7b6e77142
Jean-Marc Prieur <jmprieur@microsoft.com>
2017-07-18 10:37:49 +0200

    fixing missing square braket

--- TodoListSPA/appconfig.js
+++ TodoListSPA/appconfig.js
@@ -1,7 +1,7 @@
 // JavaScript source code
 var config = {
     tenant: "[your-tenant].onmicrosoft.com",
-    clientId: "[clientId for the TodoListSPA application, for instance: affdbdee-646b-438b-9484-71ac623ef8d6",
+    clientId: "[clientId for the TodoListSPA application, for instance: affdbdee-646b-438b-9484-71ac623ef8d6]",
     redirectUri: "http://localhost:16969/",
     popUp: true,
 }
