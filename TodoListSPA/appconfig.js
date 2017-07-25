907457c005f0fadc43172bf1f39820f1ae5e33b1
Jean-Marc Prieur <jmprieur@microsoft.com>
2017-07-25 16:08:22 +0200

    - Updating ADAL.Net to ADAL.Net 3.14.2 - Resetting the application coordinates.

--- TodoListSPA/appconfig.js
+++ TodoListSPA/appconfig.js
@@ -1,12 +1,12 @@
 // JavaScript source code
 var config = {
-    tenant: "msidentitysamplestesting.onmicrosoft.com",
-    clientId: "7cee0f68-5051-41f6-9e45-80463d21d65d",
+    tenant: "[Enter tenant name, e.g. contoso.onmicrosoft.com]",
+    clientId: "[Enter client ID as obtained from Azure Portal for this SPA, e.g. 7cee0f68-5051-41f6-9e45-80463d21d65d]",
     redirectUri: "http://localhost:16969/",
     popUp: true
 }
 
 var webApiConfig = {
-    resourceId: "https://msidentitysamplestesting.onmicrosoft.com/TodoListService-OBO",
+    resourceId: "[Enter App ID URI of TodoListService, e.g. https://contoso.onmicrosoft.com/TodoListService]",
     resourceBaseAddress: "http://localhost:9184/",
 }
